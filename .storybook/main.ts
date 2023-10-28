6219b959a762e7549e3ad90a8dba891dc933af91
Kuchuk Andrey <kreezag@gmail.com>
2023-10-28 11:36:30 +0400

    seed fsd structure

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,7 @@ module.exports = {
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
     "../layouts/**/*.stories.@(js|jsx|ts|tsx)",
     "../pages/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/**/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
