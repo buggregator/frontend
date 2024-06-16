dde0f6d34d96d3309a5b41be4046fcc9323e1913
Kreezag <kreezag@Kreezags-MacBook-Pro-2.local>
2024-06-16 22:15:11 +0400

    fix type errors in var-dump entity

--- src/entities/var-dump/lib/normalize-var-dump-event.ts
+++ src/entities/var-dump/lib/normalize-var-dump-event.ts
@@ -8,16 +8,16 @@ export const normalizeVarDumpEvent = (event: ServerEvent<VarDump>): NormalizedEv
     type: EVENT_TYPES.VAR_DUMP,
     labels: [EVENT_TYPES.VAR_DUMP],
     origin: {
-      file: event.payload.context.source.file,
-      name: event.payload.context.source.name,
-      line_number: event.payload.context.source.line,
+      file: event.payload.context?.source.file,
+      name: event.payload.context?.source.name,
+      line_number: event.payload.context?.source.line,
     },
     serverName: "",
     date: event.timestamp ? new Date(event.timestamp * 1000) : null,
     payload: event.payload
   }
 
-  if (event.payload?.payload?.label != null) {
+  if (event.payload?.payload?.label) {
     normalizedEvent.labels.push(`label: ${event.payload.payload.label}`);
   }
 
