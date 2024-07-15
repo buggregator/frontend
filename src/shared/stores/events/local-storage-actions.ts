44d1a7b2471e397fa9552298654bbfc3129e6cee
Andrei Kuchuk <kreezag@gmail.com>
2024-07-15 11:31:32 +0400

    #139 move cached and locked events storing from local to session

--- src/shared/stores/events/local-storage-actions.ts
+++ src/shared/stores/events/local-storage-actions.ts
@@ -1,10 +1,10 @@
-import {type EventId, LOCAL_STORAGE_KEYS, SESSION_STORAGE_KEYS} from "../../types";
+import {type EventId, SESSION_STORAGE_KEYS} from "../../types";
 import type {TEventsCachedIdsMap} from "./types";
 
 const { localStorage } = window;
 
 export const getStoredCachedIds = (): TEventsCachedIdsMap | null => {
-  const storageValue = localStorage?.getItem(LOCAL_STORAGE_KEYS.CACHED_EVENTS);
+  const storageValue = localStorage?.getItem(SESSION_STORAGE_KEYS.CACHED_EVENTS);
 
   if (storageValue) {
     return JSON.parse(storageValue) as TEventsCachedIdsMap;
@@ -14,12 +14,12 @@ export const getStoredCachedIds = (): TEventsCachedIdsMap | null => {
 };
 
 export const setStoredCachedIds = (cachedEventMap: TEventsCachedIdsMap) => {
-  localStorage?.setItem(LOCAL_STORAGE_KEYS.CACHED_EVENTS, JSON.stringify(cachedEventMap));
+  localStorage?.setItem(SESSION_STORAGE_KEYS.CACHED_EVENTS, JSON.stringify(cachedEventMap));
 }
 
 
 export const getStoredLockedIds = (): EventId[] | null => {
-  const storageValue = localStorage?.getItem(LOCAL_STORAGE_KEYS.LOCKED_EVENTS);
+  const storageValue = localStorage?.getItem(SESSION_STORAGE_KEYS.LOCKED_EVENTS);
 
   if (storageValue) {
     return JSON.parse(storageValue) as EventId[];
@@ -29,7 +29,7 @@ export const getStoredLockedIds = (): EventId[] | null => {
 };
 
 export const setStoredLockedIds = (lockedIds: EventId[]) => {
-  localStorage?.setItem(LOCAL_STORAGE_KEYS.LOCKED_EVENTS, JSON.stringify(lockedIds));
+  localStorage?.setItem(SESSION_STORAGE_KEYS.LOCKED_EVENTS, JSON.stringify(lockedIds));
 }
 
 export const getStoredProject = (): string | null => localStorage?.getItem(SESSION_STORAGE_KEYS.PROJECT) || null;
