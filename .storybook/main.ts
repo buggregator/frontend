662dccc2dee0e3e8b9d71c488a0e3e092b2b9da5
Andrei Kuchuk <kreezag@gmail.com>
2024-08-01 22:43:26 +0400

    try to fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -47,7 +47,7 @@ const config = {
       ...config.resolve.alias,
       '#app': fileURLToPath(new URL('../node_modules/nuxt/dist/app', import.meta.url)),
       '~/src': fileURLToPath(new URL('../src', import.meta.url)),
-      '#build': fileURLToPath(new URL('../.nuxt', import.meta.url)),
+      '#build': fileURLToPath(new URL('../.nuxt',import.meta.url )),
     }
 
     const { mergeConfig } = await import('vite');
