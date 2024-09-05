dbb60836b6b57f6aee76124c3ba966634d4780a7
Andrei Kuchuk <kreezag@gmail.com>
2024-09-05 10:43:00 +0400

    add layout-base component

---
+++ src/widgets/ui/layout-base/layout-base.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import LayoutBase from "./layout-base.vue";
+
+export default {
+  title: "Widgets/LayoutBase",
+  component: LayoutBase,
+} as Meta<typeof LayoutBase>;
+
+export const Default: StoryObj<typeof LayoutBase> = {
+  args: {
+  }
+};
---
+++ src/widgets/ui/layout-base/layout-base.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { watch } from 'vue'
+import { useEvents } from '@/shared/lib/use-events'
+import SfdumpWrap from '@/shared/lib/vendor/dumper'
+import { useEventsStore } from '@/shared/stores'
+import { LayoutSidebar } from '../layout-sidebar'
+
+// TODO: mote to pages where it is needed
+SfdumpWrap(window.document)
+
+const { activeProjectKey } = storeToRefs(useEventsStore())
+const { events } = useEvents()
+
+watch(
+  () => activeProjectKey.value,
+  () => {
+    events.getAll()
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <div class="layout-base">
+    <div class="layout-base__sidebar">
+      <slot name="sidebar">
+        <LayoutSidebar />
+      </slot>
+    </div>
+
+    <div class="layout-base__header">
+      <slot name="header" />
+    </div>
+
+    <div class="layout-base__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import 'src/assets/mixins';
+
+.layout-base {
+  @apply flex min-h-screen items-stretch relative;
+}
+
+.layout-base__sidebar {
+  @apply w-10 md:w-14 lg:w-16 flex-none border-r border-gray-200 dark:border-gray-700 z-50 w-full h-full sticky top-0 h-screen max-h-screen;
+  @include layout-sidebar;
+}
+
+.layout-base__header {
+  @include layout-head;
+}
+
+.layout-base__content {
+  @include layout-body;
+  @apply flex flex-col h-full flex-1 w-full min-h-screen absolute top-0 left-0;
+
+  & > div {
+    @apply flex flex-col h-full flex-1;
+  }
+}
+</style>
