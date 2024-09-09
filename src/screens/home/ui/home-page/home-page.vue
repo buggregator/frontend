6508577ad3836aea462757090a97fd5102e5bc93
Andrei Kuchuk <kreezag@gmail.com>
2024-09-09 21:27:43 +0400

    reduce routes for page events

--- src/screens/home/ui/home-page/home-page.vue
+++ src/screens/home/ui/home-page/home-page.vue
@@ -3,17 +3,15 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { PageHeader, LayoutPreviewEvents, LayoutBase, LayoutSidebar } from '@/widgets/ui'
 import { ALL_EVENT_TYPES, PAGES_SETTINGS } from '@/shared/constants'
-import type { PageEventTypes } from '@/shared/types'
+import { EventTypes, type PageEventTypes } from '@/shared/types'
 
 const route = useRoute()
 
-const routeName = computed(() => String(route.name) as PageEventTypes)
+const paramsType = computed(() => (route?.params?.type || undefined) as PageEventTypes | undefined)
 
-const title = computed(() => (routeName.value ? PAGES_SETTINGS[routeName.value]?.title : ''))
+const title = computed(() => (paramsType?.value ? PAGES_SETTINGS[paramsType.value]?.title : ''))
 
-const type = computed(() =>
-  routeName.value ? PAGES_SETTINGS[routeName.value]?.eventType : ALL_EVENT_TYPES
-)
+const type = computed(() => (paramsType.value as EventTypes) || ALL_EVENT_TYPES)
 </script>
 
 <template>
--- src/shared/types/app.ts
+++ src/shared/types/app.ts
@@ -1,13 +1,6 @@
 export enum RouteName {
-  VarDump = "var-dump",
-  Smtp = "smtp",
-  Sentry = "sentry",
-  Profiler = "profiler",
-  Monolog = "monolog",
-  Inspector = "inspector",
-  HttpDump = "http-dump",
-  RayDump = "ray",
   Login = 'login',
   Settings = 'settings',
   Home = 'home',
+  EventList = 'event-list',
 }
