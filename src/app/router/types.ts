991aa99fa7fd2f93cbaec11ea88dd21977e98b1f
Andrei Kuchuk <kreezag@gmail.com>
2024-09-11 20:54:03 +0400

    update router auth middlewares

--- src/app/router/types.ts
+++ src/app/router/types.ts
@@ -1,9 +1,8 @@
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
-import {RouteAuthAccessError} from "@/shared/lib/errors";
 
 export type TRouterMiddleware = (params: {
   to: RouteLocationNormalized
   from: RouteLocationNormalized
   next: NavigationGuardNext
-}) => Promise<RouteAuthAccessError | undefined>
+}) => Promise<undefined>
 
--- src/shared/lib/errors/router-errors.ts
+++ src/shared/lib/errors/router-errors.ts
@@ -1,16 +1,16 @@
-export class RouteAccessError extends Error {
+export class RouteAvailabilityError extends Error {
   constructor(message: string, routeName: string) {
-    super(message); // (1)
-    this.name = "RouteAccessError"; // (2)
+    super(message);
+    this.name = "RouteAvailabilityError";
 
-    console.error(`RouteAccessError: the route ${routeName} is not permitted`)
+    console.error(`RouteAvailabilityError: the route ${routeName} is not available`)
   }
 }
 
 export class RouteAuthAccessError extends Error {
   constructor(message: string, routeName: string) {
-    super(message); // (1)
-    this.name = "RouteAuthAccessError"; // (2)
+    super(message);
+    this.name = "RouteAuthAccessError";
 
     console.error(`RouteAuthAccessError: the route ${routeName} is not permitted without token`)
   }
