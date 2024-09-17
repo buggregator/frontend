eda3e5827012c00e05fb85b7b5fa7c95e7b59dd0
Andrei Kuchuk <kreezag@gmail.com>
2024-09-18 00:05:30 +0400

    fix ws save event to the store

--- src/app/ui/App.vue
+++ src/app/ui/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useEventsStore } from '@/shared/stores'
+
+const { initialize } = useEventsStore()
+
+initialize()
 </script>
 
 <template>
