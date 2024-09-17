0c8dcc22330d55ed30b94dc29f50851bbb3c8af5
Andrei Kuchuk <kreezag@gmail.com>
2024-09-18 00:20:36 +0400

    fix storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,7 @@ import { setup } from '@storybook/vue3';
 import "../src/assets/index.css";
 import "../src/assets/vendor";
 import "./stories.css"
-import SfdumpWrap from "../src/shared/lib/vendor/dumper";
+import { SfdumpWrap } from '../src/shared/lib/vendor/dumper'
 import 'tailwindcss/tailwind.css'
 import type { Preview } from '@storybook/vue3'
 
