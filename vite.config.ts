9b544e3c27f887409555cbc9876aa38a47af21df
Andrei Kuchuk <kreezag@gmail.com>
2024-12-22 12:06:51 +0400

    add analyzer command

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-// TODO: Fix this
-// import vueDevTools from 'vite-plugin-vue-devtools'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // vueDevTools(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
